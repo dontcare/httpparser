a5887c4998bd85321df46f8369ab0d1b887e3767
dontcare <sarginav@gmail.com>
2017-06-22 18:50:19 +0300

    Update travis

--- setup.py
+++ setup.py
@@ -77,8 +77,6 @@ setuptools.setup(
         'Programming Language :: C',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
